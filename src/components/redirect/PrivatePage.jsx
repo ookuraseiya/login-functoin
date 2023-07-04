250ed2f6fd102741558ff6c6088abdf0009e8917
ookuraseiya <seiya.ookura@gmail.com>
2023-07-04 15:01:15 +0900

    ?

--- src/components/redirect/PrivatePage.jsx
+++ src/components/redirect/PrivatePage.jsx
@@ -1,6 +1,6 @@
-//リダイレクト関数が非推奨になっているから、リダイレクト関数の代わりにナビゲート関数を使用
 import { Navigate } from 'react-router-dom';
 import { useAuthContext } from '../../context/AuthContext';
+
 export const PrivatePage = ({ children }) => {
   const { user } = useAuthContext();
   return user ? children : <Navigate to="/login" />;
